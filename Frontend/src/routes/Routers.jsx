a584ca425286fd67d0a93b141e67e7f0206eaef5
Pulkit <pulkitgangilxf24@gmail.com>
2025-03-08 07:28:45 +0530

    dashboard development

--- Frontend/src/routes/Routers.jsx
+++ Frontend/src/routes/Routers.jsx
@@ -7,6 +7,9 @@ import Login from '../pages/Login'
 import Signup from '../pages/Signup'
 import Contact from '../pages/Contact'
 import Services from '../pages/Services'
+import MyAccount from '../Dashboard/user-account/MyAccount'
+import DoctorAccount from '../Dashboard/doctor-account/DoctorAccount'
+import ProtectedRoutes from './ProtectedRoutes'
 
 const Routers = () => {
   return (
@@ -19,6 +22,8 @@ const Routers = () => {
         <Route path="/register" element={<Signup/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/services" element={<Services/>} />
+        <Route path="/users/profile/me" element={<ProtectedRoutes allowedRoles={['patient']} ><MyAccount/></ProtectedRoutes>} />
+        <Route path="/doctors/profile/me" element={<ProtectedRoutes allowedRoles={['doctor']}><DoctorAccount/></ProtectedRoutes>} />
     </Routes>
   )
 }
